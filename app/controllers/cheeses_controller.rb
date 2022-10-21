b6e1fc4a8e1c0061e19c3c9ade06a5167d5f09da
monginadiana <dianahmongina2@gmail.com>
2022-10-21 11:17:57 +0300

    param hash routing

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,8 +1,13 @@
 class CheesesController < ApplicationController
 
+ 
   def index
     cheeses = Cheese.all
     render json: cheeses
+  end
+  def show
+    cheese = Cheese.find_by(id:params[:id])
+    render json: cheese
   end
 
 end
